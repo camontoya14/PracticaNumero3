1d50f75d41e5d567ab98aa47894c3c0918f21e6a
camontoya14 <mmontoya20068@ufide.ac.cr>
2023-11-20 20:32:39 -0800

    Agregar archivos

---
+++ ApiMN/ApiMN/Controllers/TProductosController.cs
@@ -0,0 +1,131 @@
+﻿using ApiMN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiMN.Controllers
+{
+    public class TProductosController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarProductos")]
+        public List<TProducto> ConsultarProductos()
+        {
+            using (var context = new BDMNEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                return context.TProducto.ToList();
+            }
+        }
+
+        [HttpPost]
+        [Route("RegistrarProducto")]
+        public long RegistrarProducto(TProducto tProducto)
+        {
+            using (var context = new BDMNEntities())
+            {
+                context.TProducto.Add(tProducto);
+                context.SaveChanges();
+                return tProducto.ConProducto;
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarRutaImagen")]
+        public string ActualizarRutaImagen(TProducto tProducto)
+        {
+            using (var context = new BDMNEntities())
+            {
+                var datos = context.TProducto.FirstOrDefault(x => x.ConProducto == tProducto.ConProducto);
+
+                if (datos != null)
+                {
+                    datos.Imagen = tProducto.Imagen;
+                    context.SaveChanges();
+                }
+
+                return "OK";
+            }
+        }
+        [HttpGet]
+        [Route("ConsultarProductos")]
+        public TProducto ConsultarProductos(long q)
+        {
+            try
+            {
+                using (var context = new BDMNEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    var datos = (from x in context.TProducto
+                                 where x.ConProducto == q
+                                 select x).FirstOrDefault();
+
+                    return datos;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        [HttpPut]
+        [Route("ActualizarProducto")]
+        public string ActualizarProducto(ProductoEnt entidad)
+        {
+            try
+            {
+                using (var context = new BDMNEntities())
+                {
+                    var datos = (from x in context.TProducto
+                                 where x.ConProducto == entidad.ConProducto
+                                 select x).FirstOrDefault();
+
+                    if (datos != null)
+                    {
+                        datos.Nombre = entidad.Nombre;
+                        datos.Descripcion = entidad.Descripcion;
+                        datos.Precio = entidad.Precio;
+                        datos.Cantidad = entidad.Cantidad;
+                        datos.Imagen = entidad.Imagen;
+                        context.SaveChanges();
+                    }
+
+                    return "OK";
+                }
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarEstadoProducto")]
+        public string ActualizarEstadoProducto(ProductoEnt entidad)
+        {
+            try
+            {
+                using (var context = new BDMNEntities())
+                {
+                    var datos = (from x in context.TProducto
+                                 where x.ConProducto == entidad.ConProducto
+                                 select x).FirstOrDefault();
+
+                    if (datos != null)
+                    {
+                        datos.Estado = (datos.Estado == true ? false : true);
+                        context.SaveChanges();
+                    }
+
+                    return "OK";
+                }
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ WebMN/WebMN/Controllers/ProductoController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMN.Entities;
+using WebMN.Models;
+
+namespace WebMN.Controllers
+{
+    public class ProductoController : Controller
+    {
+        ProductoModel productoModel = new ProductoModel();
+
+        [HttpGet]
+        public ActionResult ConsultarProductos()
+        {
+            var datos = productoModel.ConsultarProductos();
+            return View(datos);
+        }
+
+        [HttpGet]
+        public ActionResult RegistrarProducto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarProducto(HttpPostedFileBase ImgProducto, ProductoEnt entidad)
+        {
+            entidad.Imagen = string.Empty;
+            entidad.Estado = true;
+
+            long conProducto = productoModel.RegistrarProducto(entidad);
+
+            if (conProducto > 0)
+            {
+                string extension = Path.GetExtension(Path.GetFileName(ImgProducto.FileName));
+                string ruta = AppDomain.CurrentDomain.BaseDirectory + "Images\\" + conProducto + extension;
+                ImgProducto.SaveAs(ruta);
+
+                entidad.Imagen = "/Images/" + conProducto + extension;
+                entidad.ConProducto = conProducto;
+
+                productoModel.ActualizarRutaImagen(entidad);
+
+                return RedirectToAction("ConsultarProductos", "Producto");
+            }
+            else
+            {
+                ViewBag.MensajeUsuario = "No se ha registrado el producto";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ActualizarEstadoProducto(long q)
+        {
+            var entidad = new ProductoEnt();
+            entidad.ConProducto = q;
+
+            var resp = productoModel.ActualizarEstadoProducto(entidad);
+
+            if (resp == "OK")
+            {
+                return RedirectToAction("ConsultarProductos", "Producto");
+            }
+            else
+            {
+                ViewBag.MensajeUsuario = "No se pudo actualizar el estado del producto";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ActualizarProducto(long q)
+        {
+            var datos = productoModel.ConsultarProductos(q);
+            return View(datos);
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarProducto(ProductoEnt entidad, HttpPostedFileBase NuevaImgProducto)
+        {
+            if (NuevaImgProducto != null && NuevaImgProducto.ContentLength > 0)
+            {
+                if (!string.IsNullOrEmpty(entidad.Imagen))
+                {
+                    string rutaImagenAnterior = AppDomain.CurrentDomain.BaseDirectory + entidad.Imagen.Replace("/", "\\");
+                    if (System.IO.File.Exists(rutaImagenAnterior))
+                    {
+                        System.IO.File.Delete(rutaImagenAnterior);
+                    }
+                }
+
+                string extension = Path.GetExtension(Path.GetFileName(NuevaImgProducto.FileName));
+                string rutaNuevaImagen = AppDomain.CurrentDomain.BaseDirectory + "Images\\" + entidad.ConProducto + extension;
+
+                if (System.IO.File.Exists(rutaNuevaImagen))
+                {
+                    System.IO.File.Delete(rutaNuevaImagen);
+                }
+
+                NuevaImgProducto.SaveAs(rutaNuevaImagen);
+
+                entidad.Imagen = "/Images/" + entidad.ConProducto + extension;
+            }
+
+            var resp = productoModel.ActualizarProducto(entidad);
+
+            if (resp == "OK")
+            {
+                return RedirectToAction("ConsultarProductos", "Producto");
+            }
+            else
+            {
+                ViewBag.MensajeUsuario = "No se pudo actualizar el producto";
+                return View();
+            }
+        }
+    }
+}
